269a074cf20edf3552765322aafb44b95b74d4e7
raheelshan <frommusafirshan@gmail.com>
2013-12-12 17:23:20 +0500

    Revert "Removed space from the file"
    This reverts commit c783baddbb03ef661bfeb62cc6424df10487414e.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -10,7 +10,7 @@ Class User extends MY_Controller {
 	function __construct()
 	{
  		parent::__construct();
-		$this->load->library('form_validation');
+		$this->load->library('form_validation'); 
 	}	
 	/*
 	function user()
